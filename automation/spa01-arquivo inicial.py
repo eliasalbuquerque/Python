cb58dee272acf2eb1ba169f19703b2ec5bb7a11c
eliasalbuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2021-05-01 21:28:44 -0300

    Semana python
    Aula 1, aula 2 e aula 3

---
+++ automation/spa01-arquivo inicial.py
@@ -0,0 +1,14 @@
+# importar as bibliotecas
+import pyautogui # faz a automacao do mouse e teclado
+import time # controla o tempo do nosso programa
+import pyperclip # ela permite a gente copiar e colar com o python
+
+# passo 1: Entrar no sistema (link do Google Drive)
+pyautogui.hotkey('ctrl', 't')
+
+# passo 2: Entrar na pasta da Aula 1
+# passo 3: Fazer o dawnload da Base de Vendas
+# passo 4: Calcular os indicadores (Faturamento e a quantidade de produtos)
+# passo 5: Entrar no meu email
+# passo 6: Criar o email
+# passo 7: Enviar o email
